fe53e85423c8495f8554652f10d2c72e10694d0c
ArsenySamoylov <samoylov.arseny@gmail.com>
2026-04-26 18:37:34 +0300

    rust/ray-tracer: basic Vec3 implementation

--- rust/ray-tracer/src/main.rs
+++ rust/ray-tracer/src/main.rs
@@ -1,3 +1,11 @@
+mod vec3;
+use vec3::Vec3;
+
+fn write_color(v : Vec3) {
+    let v = 255.999 * v;
+    println!("{} {} {}", v.x as u32, v.y as u32, v.z as u32)
+}
+
 fn main() {
     let width = 256;
     let height = 256;
@@ -12,11 +20,8 @@ fn main() {
             let g = (j as f64) / ((height-1)  as f64);
             let b = 0.0;
 
-            let ir = (255.999 * r) as u32;
-            let ig = (255.999 * g) as u32;
-            let ib = (255.999 * b) as u32;
-
-            println!("{} {} {}", ir, ig, ib);
+            let v = Vec3::new(r, g, b);
+            write_color(v);
         }
     }
 }
---
+++ rust/ray-tracer/src/vec3.rs
@@ -0,0 +1,70 @@
+use std::ops::Add;
+use std::ops::Mul;
+use std::ops::Div;
+
+#[derive(Clone, Copy, Debug, Default)]
+pub struct Vec3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Self {x, y, z}
+    }
+
+    pub fn length_squared(self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn length(self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    pub fn dot(self, other: Vec3) -> f64 {
+        self.x * other.x +
+        self.y * other.y +
+        self.z * other.z
+    }
+
+    pub fn unit(self) -> Vec3 {
+        self / self.length()
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: Vec3) -> Vec3 {
+        Vec3::new(
+            self.x + rhs.x, 
+            self.y + rhs.y, 
+            self.z + rhs.z,
+            )
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, t: f64) -> Vec3 {
+        Vec3::new(self.x * t, self.y * t, self.z * t)
+    }
+}
+
+impl Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, v: Vec3) -> Vec3 {
+        v * self
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, t: f64) -> Vec3 {
+        Vec3::new(self.x / t, self.y / t, self.z / t)
+    }
+}
