d1b905ff1f6e1e988b032ab3b4e769506ecd3f66
ArsenySamoylov <samoylov.arseny@gmail.com>
2026-04-26 21:13:08 +0300

    rust/ray-tracer: Simple sphere rendering

--- rust/ray-tracer/src/main.rs
+++ rust/ray-tracer/src/main.rs
@@ -10,11 +10,30 @@ fn write_color(v: Vec3) {
 }
 
 fn ray_color(r: Ray) -> Vec3 {
+    let t = hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0{
+        let n = (r.at(t) - Vec3::new(0.0, 0.0, -1.0)).unit();
+        return 0.5 * Vec3::new(n.x+1.0, n.y + 1.0, n.z + 1.0)
+    }
+
     let unit_direction = r.direction.unit();
     let a = 0.5 * (unit_direction.y + 1.0);
     return (1.0 - a)*Vec3::new(1.0, 1.0, 1.0) + a*Vec3::new(0.5, 0.7, 1.0);
 }
 
+fn hit_sphere(center: Vec3, radius: f64, ray: Ray) -> f64 {
+    let oc = center - ray.origin;
+    let a = ray.direction.length_squared();
+    let h = ray.direction.dot(oc);
+    let c = oc.length_squared() - radius * radius;
+    let discriminant = h*h - a*c;
+
+    if discriminant < 0.0 {
+        return -1.0;
+    }
+
+    return (h - discriminant.sqrt()) / a;
+}
 fn main() {
     // Image
 
