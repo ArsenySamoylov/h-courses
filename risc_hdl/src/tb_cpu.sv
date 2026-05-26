89dc12a7e9d9f73c6be9b96faaebe23b0446c04c
ArsenySamoylov <samoylov.arseny@gmail.com>
2026-05-26 19:28:20 +0300

    risc_hdl: small test improvement

--- risc_hdl/src/tb_cpu.sv
+++ risc_hdl/src/tb_cpu.sv
@@ -91,25 +91,61 @@ module tb_cpu;
             dmem[i] = 0;
         end
 
+        // fetch:  pc = 0
+        // decode: pc = 4
+        // exec:   pc = 8
+        // mem:    pc = 12
+        // wb:     pc = 16
         // addi x1, x0, 5
         imem[0] = 32'h00500093;
 
+        // fetch:  pc = 4
+        // decode: pc = 8
+        // exec:   pc = 12
+        // mem:    pc = 16
+        // wb:     pc = 20
         // addi x2, x0, 10
         imem[1] = 32'h00A00113;
 
+        // fetch:  pc = 8
+        // decode: pc = 12
+        // exec:   pc = 16
+        // mem:    pc = 20
+        // wb:     pc = 24
         // add x3, x1, x2
         imem[2] = 32'h002081B3;
 
+        // fetch:  pc = 12
+        // decode: pc = 16
+        // exec:   pc = 20
+        // mem:    pc = 24
+        // wb:     pc = 28
         // sub x4, x3, x1
-        // Note: here we must forward x3 value from previous instruction
+        // Note: here we must forward x3 value from EX stage
         imem[3] = 32'h40118233;
 
+        // fetch:  pc = 16
+        // decode: pc = 20
+        // exec:   pc = 24
+        // mem:    pc = 28
+        // wb:     pc = 32
         // sw x4, 0(x0)
+        // Note: here we must forward x4 value from EX stage
         imem[4] = 32'h00402023;
 
+        // fetch:  pc = 20
+        // decode: pc = 24
+        // exec:   pc = 28
+        // mem:    pc = 32
+        // wb:     pc = 36
         // lw x5, 0(x0)
         imem[5] = 32'h00002283;
 
+        // fetch:  pc = 24
+        // decode: pc = 28
+        // exec:   pc = 32
+        // mem:    pc = 39
+        // wb:     pc = 40
         // add x6, x5, x1
         imem[6] = 32'h00128333;
 
@@ -132,22 +168,15 @@ module tb_cpu;
         repeat (30)
             @(posedge clk);
 
-        $display("Checking register state...");
+        $display("Checking register and memory state...");
 
         check_reg(1, 5);
         check_reg(2, 10);
         check_reg(3, 15);
         check_reg(4, 10);
+        check_mem(0, 10);
         check_reg(5, 10);
         check_reg(6, 15);
-
-        assert(dmem[0] == 10)
-        else begin
-            $fatal(1,
-                "DMEM[0] FAIL got=%0d expected=10",
-                dmem[0]
-            );
-        end
 
         $display("=================================");
         $display("ALL TESTS PASSED");
@@ -181,6 +210,33 @@ module tb_cpu;
 
         $display(
             "PASS x%0d = %0d",
+            idx,
+            actual
+        );
+    end
+    endtask
+
+    task automatic check_mem(
+        input int idx,
+        input int expected
+    );
+        int actual;
+    begin
+
+        actual = dmem[idx];
+
+        assert(actual == expected)
+        else begin
+            $fatal(1,
+                "MEM FAIL x%0d got=%0d expected=%0d",
+                idx,
+                actual,
+                expected
+            );
+        end
+
+        $display(
+            "PASS dmem[%0d] = %0d",
             idx,
             actual
         );
