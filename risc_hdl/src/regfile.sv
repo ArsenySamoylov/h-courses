9932edf11016f6ddac7f705e2306be7c64ba14f8
ArsenySamoylov <samoylov.arseny@gmail.com>
2026-06-02 12:55:16 +0300

    [risc_hdl] update regfile on negative edge

--- risc_hdl/src/regfile.sv
+++ risc_hdl/src/regfile.sv
@@ -16,22 +16,16 @@ module regfile (
 
     logic [31:0] regs [31:0];
 
-    always_ff @(posedge clk) begin
+    always_ff @(negedge clk) begin
         if (WB_WE && (WB_A != 5'd0))
             regs[WB_A] <= WB_D;
-
-        regs[0] <= 32'b0;
     end
 
     // Note: We must explicitly handle when writting to the register, we are reading from.
     assign D1 =
-        (A1 == 5'd0) ? 32'b0 : // return 0 for x0 reg
-        (WB_WE && (WB_A == A1) && (WB_A != 0)) ? WB_D : // forward write
-        regs[A1];
+        (A1 == 5'd0) ? 32'b0 :regs[A1];
 
     assign D2 =
-        (A2 == 5'd0) ? 32'b0 :
-        (WB_WE && (WB_A == A2) && (WB_A != 0)) ? WB_D :
-        regs[A2];
+        (A2 == 5'd0) ? 32'b0 :regs[A2];
 
 endmodule
